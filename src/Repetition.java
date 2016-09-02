aa1980aa21843d93deb226728c38a4101f983993
leca14 <te14.carl.leong@umea.nti.se>
2016-09-02 11:23:53 +0200

    Har fyra fel och en coka cola, men det kunde varit värre.

--- src/Repetition.java
+++ src/Repetition.java
@@ -2,5 +2,20 @@
  * Created by leca14 on 2016-09-02.
  */
 public class Repetition {
-    
+    public static void main(String[]args){
+       int[] jazzArray;
+
+        jazzArray = new int[3];
+
+        jazzArray[0] = 80;
+        jazzArray[1] = 85;
+        jazzArray[2] = 90;
+        jazzArray[3] = 100;
+
+        System.out.println("Elements in Jazz");
+        for (jazzArray = 0; jazzArray < jazzArray.length; jazzArray++){
+            System.out.println(jazzArray[jazzArray]);
+        }
+
+    }
 }
